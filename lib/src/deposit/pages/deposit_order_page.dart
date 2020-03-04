52c1068703bd2e099156647bfc819b354057ae24
asi <jsasi@itcom888.com>
2020-03-04 19:20:42 +0800

    [新增功能](v0.1): 订单详情

--- lib/src/deposit/pages/deposit_order_page.dart
+++ lib/src/deposit/pages/deposit_order_page.dart
@@ -4,6 +4,7 @@ import 'package:bw_res/bw_res.dart';
 import 'package:bw_sponsor_preferential/bw_sponsor_preferential.dart';
 import 'package:bw_sponsor_preferential/src/deposit/model/deposit_pay_entity.dart';
 import 'package:bw_sponsor_preferential/src/widgets/dss_app_bar.dart';
+import 'package:bw_sponsor_preferential/src/widgets/simple_imageview.dart';
 import 'package:flutter/material.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
@@ -50,7 +51,8 @@ class _DepositOrderPageState extends State<DepositOrderPage> {
 
   //倒计时
   void _addICountDownListener() {
-    _streamCountDown = Stream<int>.periodic(Duration(seconds: 1800));
+    _streamCountDown =
+        Stream<int>.periodic(Duration(seconds: int.parse(_data.expierTime)));
     _streamCountDown.listen((data) {
       print("===data=====$data");
       _getountDownTxt(int.parse(_data.expierTime) - data);
@@ -116,10 +118,21 @@ class _DepositOrderPageState extends State<DepositOrderPage> {
                     padding: EdgeInsets.only(top: 12),
                     color: Colors.white,
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Text('请在',style: TextStyle(fontSize: 12,color: BWColors.depOrdDesTxt),),
-                        Text('${snapshot.data}',style: TextStyle(fontSize: 12,color: BWColors.depOrdCountDown)),
-                        Text('内完成支付',style: TextStyle(fontSize: 12,color: BWColors.depOrdDesTxt),),
+                        Text(
+                          '请在',
+                          style: TextStyle(
+                              fontSize: 12, color: BWColors.depOrdDesTxt),
+                        ),
+                        Text('${snapshot.data}',
+                            style: TextStyle(
+                                fontSize: 12, color: BWColors.depOrdCountDown)),
+                        Text(
+                          '内完成支付',
+                          style: TextStyle(
+                              fontSize: 12, color: BWColors.depOrdDesTxt),
+                        ),
                       ],
                     ),
                   );
@@ -130,143 +143,213 @@ class _DepositOrderPageState extends State<DepositOrderPage> {
               padding: EdgeInsets.only(top: 11, bottom: 18),
               color: Colors.white,
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text('如已完成支付，请',style: TextStyle(fontSize: 12,color: BWColors.depOrdDesTxt)),
+                  Text('如已完成支付，请',
+                      style: TextStyle(
+                          fontSize: 12, color: BWColors.depOrdDesTxt)),
                   InkWell(
                       onTap: () => Navigator.of(context)
                           .pushNamed(Routes.customerService),
-                      child: Text(' 联系客服 ',style: TextStyle(fontSize: 12,color: BWColors.depOrdColor))),
-                  Text('确认',style: TextStyle(fontSize: 12,color: BWColors.depOrdDesTxt)),
+                      child: Text(' 联系客服 ',
+                          style: TextStyle(
+                              fontSize: 12, color: BWColors.depOrdColor))),
+                  Text('确认',
+                      style: TextStyle(
+                          fontSize: 12, color: BWColors.depOrdDesTxt)),
                 ],
               ),
             ),
-            if(_data.trnasfer=="0")
-            Card(
+            if (_data.trnasfer == "0")
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.all(14),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 20, left: 12, right: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('存款方式',
+                              style: TextStyle(
+                                  fontSize: 14, color: BWColors.depOrdDesTxt)),
+                          Text(_data.payTypeName ?? "",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: BWColors.depOrdValueTxt)),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 15, left: 12, right: 12, bottom: 15),
+                      child: Row(
+                        children: <Widget>[
+                          Text('订单号码',style: TextStyle(
+                              fontSize: 14, color: BWColors.depOrdDesTxt)),
+                          Expanded(
+                              child: Text('${_data.billNo}',
+                                  textDirection: TextDirection.rtl,
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: BWColors.depOrdValueTxt))),
+                          IconButton(
+                            iconSize: 12,
+                            alignment: Alignment.centerRight,
+                            padding: EdgeInsets.all(0),
+                            onPressed: () {
+                              //todo
+                            },
+                            icon: Icon(
+                              Icons.content_copy,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            if (_data.trnasfer == "1")
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.all(14),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20,right: 20,top:20,bottom: 34 ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          SimpleImageView.display(_data.icon,
+                              width: 24, height: 24),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 7),
+                            child: Text('${_data.bankName}',
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.bold,color: BWColors.depInput)),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Row(
+                          children: <Widget>[
+                            Text('姓名：',style: TextStyle(
+                                fontSize: 14, color: BWColors.depOrdDesTxt)),
+                            Expanded(child: Text('${_data.recipientName}',style: TextStyle(fontSize: 12,color: BWColors.depOrdValueTxt),)),
+                            Container(
+                              width: 34,
+                              height: 15,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(2),
+                                border: Border.all(color: BWColors.depOrdColor),
+                              ),
+                              child: Text('复制',style: TextStyle(fontSize: 12,color: BWColors.depOrdColor,height: 1),),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Row(
+                          children: <Widget>[
+                            Text('金额：',style: TextStyle(
+                                fontSize: 14, color: BWColors.depOrdDesTxt)),
+                            Expanded(child: Text('${_data.amount}元',style: TextStyle(fontSize: 12,color: BWColors.depOrdValueTxt))),
+                            Container(
+                              width: 34,
+                              height: 15,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                border: Border.all(color: BWColors.depOrdColor),
+                                borderRadius: BorderRadius.circular(2),
+                              ),
+                              child: Text('复制',style: TextStyle(fontSize: 12,color: BWColors.depOrdColor,height: 1),),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        '(转账金额务必于订单金额一致)',
+                        style: TextStyle(fontSize: 12, color: BWColors.depError),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Row(
+                          children: <Widget>[
+                            Text('地址：',style: TextStyle(
+                                fontSize: 14, color: BWColors.depOrdDesTxt)),
+                            Expanded(child: Text('${_data.recipientAddress}',style: TextStyle(fontSize: 12,color: BWColors.depOrdValueTxt))),
+                            Container(
+                              width: 34,
+                              height: 15,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(2),
+                                border: Border.all(color: BWColors.depOrdColor),
+                              ),
+                              child: Text('复制',style: TextStyle(fontSize: 12,color: BWColors.depOrdColor,height: 1),),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Row(
+                          children: <Widget>[
+                            Text('附言/备注：',style: TextStyle(
+                                fontSize: 14, color: BWColors.depOrdDesTxt)),
+                            Expanded(child: Text('${_data.remark}',style: TextStyle(fontSize: 12,color: BWColors.depOrdValueTxt))),
+                            Container(
+                              width: 34,
+                              height: 15,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(2),
+                                border: Border.all(color: BWColors.depOrdColor),
+                              ),
+                              child: Text('复制',style: TextStyle(fontSize: 12,color: BWColors.depOrdColor,height: 1),),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        '((请务必填写正确的附言/备注))',
+                        style: TextStyle(fontSize: 12, color: BWColors.depError),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            if (_data.trnasfer == "1")
+              Card(
               color: Colors.white,
               margin: EdgeInsets.all(14),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Text('存款方式'),
-                      Text('支付宝扫码'),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Text('订单号码'),
-                      Text('PY201906104412547SMGJD5'),
-                      Icon(
-                        Icons.content_copy,
-                        size: 12,
-                      ),
-                    ],
-                  ),
-                ],
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('重要提醒',style: TextStyle(fontSize: 14,color: BWColors.depOrdValueTxt),),
+                    Text('1.转账金额须与订单金额一致，否则存款无法及时到账',style: TextStyle(fontSize: 12,color: BWColors.depOrdDesTxt,height: 1.4),),
+                    Text('2.转账必须填写正确附言（备注），否则存款无法及时到账',style: TextStyle(fontSize: 12,color: BWColors.depOrdDesTxt,height: 1.4),),
+                    Text('3.下次存款时，请获取新的账号，存入旧账号将无法到账',style: TextStyle(fontSize: 12,color: BWColors.depOrdDesTxt,height: 1.4),),
+                    Text('4.请及时前往存款，附言（备注）有效时间只有15分钟',style: TextStyle(fontSize: 12,color: BWColors.depOrdDesTxt,height: 1.4),),
+                    Text('5.请勿使用支付宝、微信转账至公司账户',style: TextStyle(fontSize: 12,color: BWColors.depOrdDesTxt,height: 1.4),),
+                    Text('6.若存款存在疑问，请及时联系客服',style: TextStyle(fontSize: 12,color: BWColors.depOrdDesTxt,height: 1.4),),
+                    Text('7.不知如何存款，请移步至 存款教程',style: TextStyle(fontSize: 12,color: BWColors.depOrdDesTxt,height: 1.4),),
+                  ],
+                ),
               ),
             ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.all(14),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.camera,
-                        size: 24,
-                      ),
-                      Text('建设银行'),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Text('姓名：'),
-                      Text('周杰伦'),
-                      Container(
-                        width: 34,
-                        height: 15,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(2),
-                          border: Border.all(color: BWColors.depOrdColor),
-                        ),
-                        child: Text('复制'),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Text('金额：'),
-                      Text('100元'),
-                      Container(
-                        width: 34,
-                        height: 15,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: BWColors.depOrdColor),
-                          borderRadius: BorderRadius.circular(2),
-                        ),
-                        child: Text('复制'),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    '(转账金额务必于订单金额一致)',
-                    style: TextStyle(fontSize: 12, color: BWColors.depError),
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Text('地址：'),
-                      Text('北京'),
-                      Container(
-                        width: 34,
-                        height: 15,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: BWColors.depOrdColor),
-                          borderRadius: BorderRadius.circular(2),
-                        ),
-                        child: Text('复制'),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Text('附言/备注：'),
-                      Text('123'),
-                      Container(
-                        width: 34,
-                        height: 15,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: BWColors.depOrdColor),
-                          borderRadius: BorderRadius.circular(2),
-                        ),
-                        child: Text('复制'),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    '((请务必填写正确的附言/备注))',
-                    style: TextStyle(fontSize: 12, color: BWColors.depError),
-                  ),
-                ],
-              ),
+            Padding(
+              padding: const EdgeInsets.only(top: 6,bottom: 40),
+              child: Text('取消存款申请'),
             ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.all(14),
-              child: Column(
-                children: <Widget>[
-                  Text('重要提醒'),
-                  Text('1.转账金额须与订单金额一致，否则存款无法及时到账'),
-                  Text('2.转账必须填写正确附言（备注），否则存款无法及时到账'),
-                  Text('3.下次存款时，请获取新的账号，存入旧账号将无法到账'),
-                  Text('4.请及时前往存款，附言（备注）有效时间只有15分钟'),
-                  Text('5.请勿使用支付宝、微信转账至公司账户'),
-                  Text('6.若存款存在疑问，请及时联系客服'),
-                  Text('7.不知如何存款，请移步至 存款教程'),
-                ],
-              ),
-            ),
-            Text('取消存款申请'),
           ],
         ),
       ),
