ad1a95c093497c1c90920b7f3df97e29db16b973
lex <>
2020-03-04 17:44:45 +0800

    export pages

--- lib/bw_sponsor_preferential.dart
+++ lib/bw_sponsor_preferential.dart
@@ -1,3 +1,6 @@
 library bw_sponsor_preferential;
 
 export 'src/routers/routes.dart';
+export 'src/sponsor/pages/discount/discount_page.dart';
+export 'src/sponsor/pages/sponsor/sponsor_page.dart';
+export 'src/sponsor/pages/service/home/service_page.dart';
