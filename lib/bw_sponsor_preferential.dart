c95896a4555f80034c7c2534e35a1026eff80b87
lex <>
2020-03-05 22:19:07 +0800

    export 'src/routers/sponsor_preferential_route.dart';

--- lib/bw_sponsor_preferential.dart
+++ lib/bw_sponsor_preferential.dart
@@ -4,3 +4,4 @@ export 'src/routers/sponsor_preferential_route.dart';
 export 'src/sponsor/pages/discount/discount_page.dart';
 export 'src/sponsor/pages/sponsor/sponsor_page.dart';
 export 'src/sponsor/pages/service/home/service_page.dart';
+export 'src/routers/sponsor_preferential_route.dart';
