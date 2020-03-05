757aca5c1f82f02b1d24ac9a5c3086abdf2392c7
lex <>
2020-03-05 15:13:04 +0800

    add navigatorKey in MaterialApp

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bw_base/bw_base.dart';
 import 'package:bw_sponsor_preferential/bw_sponsor_preferential.dart';
 import 'package:flutter/material.dart';
 import 'package:oktoast/oktoast.dart';
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return OKToast(
       child: MaterialApp(
+        navigatorKey: bwManager.navigatorKey,
         title: 'Flutter Demo',
         theme: ThemeData(
             primarySwatch: Colors.blue,
